42f5aaa27d919b7a8fc28594442de87e857e25ed
Maarten van Diemen <1440089+maartenvandiemen@users.noreply.github.com>
2024-11-03 22:27:27 +0100

    Add code to parse command line arguments and update XML elements
    * Parse command line arguments and assign values to variables.
    * Find and update the `SofiNr`, `Voorl`, `Voorv`, `SignNm`, `Gebdat`, and `AantVerlUPens` elements in the XML.
    * Save the modified XML to the output file.
    * Add the provided XML template content to `template.xml`.

--- upa-kreator/Program.cs
+++ upa-kreator/Program.cs
@@ -11,6 +11,19 @@ namespace UpaKreator
             string templateFilePath = "template.xml";
             string outputFilePath = "output.xml";
 
+            if (args.Length < 6)
+            {
+                Console.WriteLine("Please provide all required parameters: sofinr, voorl, voorv, surname, gebdat, aantalVerloondeUren");
+                return;
+            }
+
+            string sofinr = args[0];
+            string voorl = args[1];
+            string voorv = args[2];
+            string surname = args[3];
+            string gebdat = args[4];
+            string aantalVerloondeUren = args[5];
+
             if (!File.Exists(templateFilePath))
             {
                 Console.WriteLine("Template file not found.");
@@ -29,6 +42,42 @@ namespace UpaKreator
             if (datTdAanmElement != null)
             {
                 datTdAanmElement.Value = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            }
+
+            XElement sofiNrElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "SofiNr");
+            if (sofiNrElement != null)
+            {
+                sofiNrElement.Value = sofinr;
+            }
+
+            XElement voorlElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "Voorl");
+            if (voorlElement != null)
+            {
+                voorlElement.Value = voorl;
+            }
+
+            XElement voorvElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "Voorv");
+            if (voorvElement != null)
+            {
+                voorvElement.Value = voorv;
+            }
+
+            XElement signNmElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "SignNm");
+            if (signNmElement != null)
+            {
+                signNmElement.Value = surname;
+            }
+
+            XElement gebdatElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "Gebdat");
+            if (gebdatElement != null)
+            {
+                gebdatElement.Value = gebdat;
+            }
+
+            XElement aantVerlUPensElement = xmlDoc.Descendants().FirstOrDefault(e => e.Name.LocalName == "AantVerlUPens");
+            if (aantVerlUPensElement != null)
+            {
+                aantVerlUPensElement.Value = aantalVerloondeUren;
             }
 
             xmlDoc.Save(outputFilePath);
